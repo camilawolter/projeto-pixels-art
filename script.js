3571ed4c6921bbdc3de5eb57a213cee55ef7bf9a
Camila Wolter <camila0wolter@gmail.com>
2022-05-25 19:14:59 -0300

    requisito 8

--- script.js
+++ script.js
@@ -15,16 +15,29 @@ function corSelecionada() {
 }
 corSelecionada();
 
-//Selecionando uma cor***
+//Selecionando uma cor
 const corPaleta = document.querySelectorAll('.color');
 function selecionaCor(evento) {
     const selecionada = document.querySelector('.selected');
     selecionada.classList.remove('selected');
     evento.target.classList.add('selected');
   }
-      // Referência: https://blog.betrybe.com/javascript/javascript-foreach/
+                                                      // Referência:forEach - https://blog.betrybe.com/javascript/javascript-foreach/
   corPaleta.forEach(function(evento){
     evento.addEventListener('click', selecionaCor)
 });
 
-
+//Preenche um pixel da cor selecionada
+function selecionaPinta() {
+    const pixel = document.querySelectorAll('.pixel');
+    for (let index = 0; index < pixel.length; index += 1) {
+        pixel[index].addEventListener('click', function(evento) {
+            const qualSelecionado = document.querySelector('.selected');
+                                                                          //Referências: .getComputedStyle - https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/getPropertyValue 
+                                                                          //.getPropertyValue -  https://www.w3schools.com/jsref/jsref_getcomputedstyle.asp
+            const corSelecionadaEAplicando = window.getComputedStyle(qualSelecionado, null).getPropertyValue('background-color');
+            evento.target.style.backgroundColor = corSelecionadaEAplicando;
+        })
+    }
+}
+selecionaPinta();
