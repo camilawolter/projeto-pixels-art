75d1edd589ec55de26446724f359627a978b361d
Camila Wolter <camila0wolter@gmail.com>
2022-05-25 17:20:21 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const corUm = document.getElementsByClassName('color')[1];
-corUm.style.background = 'pink';
+//Adicionando cores na paleta
+const corDois = document.getElementsByClassName('color')[1];
+corDois.style.background = 'pink';
 
-const corDois = document.getElementsByClassName('color')[2];
-corDois.style.background = 'green';
+const corTres = document.getElementsByClassName('color')[2];
+corTres.style.background = 'green';
 
-const corTres = document.getElementsByClassName('color')[3];
-corTres.style.background = 'yellow';
+const corQuatro = document.getElementsByClassName('color')[3];
+corQuatro.style.background = 'yellow';
+
+//Selecionando a cor incial
+
+function corSelecionada() {
+    const corUm = document.querySelector('.color');
+    corUm.classList.add('selected');
+}
+corSelecionada();
