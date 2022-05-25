2b4a5b766f141fd74fdbf3229a5ba1df18a4b46f
Camila Wolter <camila0wolter@gmail.com>
2022-05-25 15:44:43 -0300

    resquisitos 2 e 3

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function coresPaleta (cor) {
-    let paletaContainer = document.querySelector('.color');
-  
-    paletaContainer.style.backgroundColor = cor;
-    
-  }
-  coresPaleta('black');
+let corUm = document.getElementsByClassName('color')[1];
+corUm.style.background = 'pink';
+
+let corDois = document.getElementsByClassName('color')[2];
+corDois.style.background = 'green';
+
+let corTres = document.getElementsByClassName('color')[3];
+corTres.style.background = 'yellow';
