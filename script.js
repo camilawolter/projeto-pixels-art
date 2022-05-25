f21218ed99ed15f3b0b29f026396c6da05e95f40
Camila Wolter <camila0wolter@gmail.com>
2022-05-25 16:38:45 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let corUm = document.getElementsByClassName('color')[1];
+const corUm = document.getElementsByClassName('color')[1];
 corUm.style.background = 'pink';
 
-let corDois = document.getElementsByClassName('color')[2];
+const corDois = document.getElementsByClassName('color')[2];
 corDois.style.background = 'green';
 
-let corTres = document.getElementsByClassName('color')[3];
+const corTres = document.getElementsByClassName('color')[3];
 corTres.style.background = 'yellow';
